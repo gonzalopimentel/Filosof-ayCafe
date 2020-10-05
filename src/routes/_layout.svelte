eb68b77bc185cb962d1beb52287187ccb3416606
Gonzalo Pimentel <gonzalo.pimentel.ramirez@gmail.com>
2020-10-05 09:12:01 +0200

    Header, Nav y Main

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Header from "../components/Header.svelte";
+  import Nav from "../components/Nav.svelte";
+  import Main from "../components/Main.svelte";
+  export let segment;
+</script>
+
+<style>
+</style>
+
+<Header />
+<Nav {segment} />
+
+<Main>
+  <slot />
+</Main>
---
+++ src/routes/about.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<div class="About">
+  <h1>Sobre el sitio</h1>
+  <p>
+    Este es un sitio donde compartimos lecturas interesantes sobre Filosofía.
+    Alimenta a tu alma y mente y por qué no, mientras tomas un café.
+  </p>
+</div>
